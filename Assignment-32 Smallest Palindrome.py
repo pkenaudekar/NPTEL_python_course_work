f6a14854f1930b68d17d0c07da97886a080fd6d6
Pratik Kenaudekar <34656896+pkenaudekar@users.noreply.github.com>
2019-04-17 01:48:25 +0530

    Add files via upload

---
+++ Assignment-32 Smallest Palindrome.py
@@ -0,0 +1,49 @@
+"""
+Given a string S having characters from English alphabets ['a' - 'z'] and '.' as the special character (without quotes). 
+Write a program to construct the lexicographically smallest palindrome by filling each of the faded character ('.') with a lower case alphabet.
+Definition:
+The smallest lexicographical order is an order relation where string s is smaller than t, given the first character of s (s1 ) is smaller than the first character of t (t1 ), or in case they
+are equivalent, the second character, etc.
+For example "aaabbb" is smaller than "aaac" because although the first three characters
+are equal, the fourth character b is smaller than the fourth character c. 
+Input Format: 
+String S
+Output Format: 
+Print lexicographically smallest palindrome after filling each '.' character, if it
+possible to construct one. Print -1 otherwise.
+Example-1
+Input:
+a.ba
+Output:
+abba
+Example-2:
+Input:
+a.b
+Output:
+-1
+Explanation: 
+In example 1, you can create a palindrome by filling the '.' character by 'b'.
+In example 2, it is not possible to make the string s a palindrome.
+"""
+
+#CODE:
+S = input()
+length = len(S)
+def SmallestPalindrome(S, length):
+    i = 0
+    j = length - 1
+    S = list(S)
+    while (i <= j):  
+        if (S[i] == S[j] == '.'): 
+            S[i] = S[j] = 'a'  
+        elif S[i] != S[j]:  
+            if (S[i] == '.'):
+                S[i] = S[j]
+            elif (S[j] == '.'):
+                S[j] = S[i]
+            else:  
+                return -1
+        i = i + 1  
+        j = j - 1 
+    return "".join(S) 
+print(SmallestPalindrome(S, length)) 
